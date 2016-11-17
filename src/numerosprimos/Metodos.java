ab17801d9d7bba430823df319719fa114415d69a
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2016-11-17 09:25:24 +0100

    metodo primos

--- src/numerosprimos/Metodos.java
+++ src/numerosprimos/Metodos.java
@@ -6,6 +6,7 @@
 package numerosprimos;
 
 import javax.swing.JOptionPane;
+import static numerosprimos.NumerosPrimos.p;
 
 /**
  *
@@ -15,7 +16,38 @@ public class Metodos {
         public static int pedirNumDigitos(){
          int numDigitos=Integer.parseInt(JOptionPane.showInputDialog("introducir numero"));
         if(numDigitos<=0)
-        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
+         do{
+       numDigitos=Integer.parseInt(JOptionPane.showInputDialog("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): "));
+        } while (numDigitos<=0);
         return numDigitos;
     }
+        
+        public void primos(int ndigitos,int numDigitos,int i){
+            if(ndigitos==numDigitos){
+                   if (i < 4) p = true;
+            else
+            {
+            if (i % 2 == 0) p = false; 
+                else
+                {
+                    int contador1 = 0;  
+                    int i1 = 1; 
+                    int limite = (i - 1) / 2; 
+                    if  (limite % 2 == 0) limite--;  
+             
+                    while(i1 <= limite)
+                    {
+                        if (i % i1 == 0) contador1++;
+                            i1 += 2;
+                            if (contador1 == 2) i1 = limite + 1;
+                    }
+ 
+        if (contador1 == 1) p = true;
+            } 
+        } 
+     
+                if (p == true) 
+                    System.out.println(i);    
+            }
+        }
 }
