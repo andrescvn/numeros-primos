e3600f4883da4cd4245108a4a96ced74879cf8c4
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2016-11-17 10:06:23 +0100

    cambio nombre variable p

--- src/numerosprimos/Metodos.java
+++ src/numerosprimos/Metodos.java
@@ -6,47 +6,51 @@
 package numerosprimos;
 
 import javax.swing.JOptionPane;
-import static numerosprimos.NumerosPrimos.p;
+import static numerosprimos.NumerosPrimos.primo;
 
 /**
  *
  * @author acomesanavila
  */
 public class Metodos {
-        public static int pedirNumDigitos(){
-         int numDigitos=Integer.parseInt(JOptionPane.showInputDialog("introducir numero"));
-        if(numDigitos<=0)
-         do{
-       numDigitos=Integer.parseInt(JOptionPane.showInputDialog("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): "));
-        } while (numDigitos<=0||numDigitos>5);
+
+    public static int pedirNumDigitos() {
+        int numDigitos = Integer.parseInt(JOptionPane.showInputDialog("introducir numero"));
+        while (numDigitos <= 0 || numDigitos > 5) 
+            numDigitos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): "));
         return numDigitos;
     }
-        
-        public void primos(int ndigitos,int numDigitos,int i){
-            if(ndigitos==numDigitos){
-            if (i < 4) 
-                       p = true;
-            else{if (i % 2 == 0) 
-                p = false; 
-            else{
-                    int contador1 = 0;  
-                    int i1 = 1; 
-                    int limite = (i - 1) / 2; 
-                    if  (limite % 2 == 0) limite--;  
-             
-                    while(i1 <= limite){
-                        if (i % i1 == 0) contador1++;
-                            i1 += 2;
-                            if (contador1 == 2) i1 = limite + 1;
+
+    public void primos(int ndigitos, int numDigitos, int i) {
+        if (ndigitos == numDigitos) {
+            if (i < 4) {
+                primo = true;
+            } else if (i % 2 == 0) {
+                primo = false;
+            } else {
+                int contador = 0;
+                int i1 = 1;
+                int limite = (i - 1) / 2;
+                if (limite % 2 == 0) {
+                    limite--;
+                }
+                while (i1 <= limite) {
+                    if (i % i1 == 0) {
+                        contador++;
                     }
- 
-            if (contador1 == 1) 
-            p = true;
-            } 
-        } 
-     
-                if (p == true) 
-                    System.out.println(i);    
+                    i1 += 2;
+                    if (contador == 2) {
+                        i1 = limite + 1;
+                    }
+                }
+
+                if (contador == 1) {
+                    primo = true;
+                }
+            }
+            if (primo == true) {
+                System.out.println(i);
             }
         }
+    }
 }
--- src/numerosprimos/NumerosPrimos.java
+++ src/numerosprimos/NumerosPrimos.java
@@ -1,18 +1,17 @@
 package numerosprimos;
 public class NumerosPrimos {
-    public static boolean p = false;
-    public static void main(String arg[])
-        {
+    public static boolean primo = false;
+    public static void main(String arg[]) {
         Metodos met = new Metodos();
-        int numDigitos=met.pedirNumDigitos();
-        for(int i = 1; i <= 99999; i++ ){
+        int numDigitos = met.pedirNumDigitos();
+        for (int i = 1; i <= 99999; i++) {
             int divisionEntera = i;
-            int ndigitos=0;
-            while (divisionEntera != 0){
-            divisionEntera = divisionEntera / 10;
-            ndigitos++;
-        }  
-        met.primos(ndigitos, numDigitos, i);
+            int ndigitos = 0;
+            while (divisionEntera != 0) {
+                divisionEntera = divisionEntera / 10;
+                ndigitos++;
+            }
+            met.primos(ndigitos, numDigitos, i);
         }
-        }
+    }
 }
