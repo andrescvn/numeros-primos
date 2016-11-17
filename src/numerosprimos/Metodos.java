9724e25743840f8d676955a75b3c46457c14c564
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2016-11-17 09:38:00 +0100

    cambios condicion pedirnumdigitos

--- src/numerosprimos/Metodos.java
+++ src/numerosprimos/Metodos.java
@@ -18,31 +18,30 @@ public class Metodos {
         if(numDigitos<=0)
          do{
        numDigitos=Integer.parseInt(JOptionPane.showInputDialog("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): "));
-        } while (numDigitos<=0);
+        } while (numDigitos<=0||numDigitos>5);
         return numDigitos;
     }
         
         public void primos(int ndigitos,int numDigitos,int i){
             if(ndigitos==numDigitos){
-                   if (i < 4) p = true;
-            else
-            {
-            if (i % 2 == 0) p = false; 
-                else
-                {
+            if (i < 4) 
+                       p = true;
+            else{if (i % 2 == 0) 
+                p = false; 
+            else{
                     int contador1 = 0;  
                     int i1 = 1; 
                     int limite = (i - 1) / 2; 
                     if  (limite % 2 == 0) limite--;  
              
-                    while(i1 <= limite)
-                    {
+                    while(i1 <= limite){
                         if (i % i1 == 0) contador1++;
                             i1 += 2;
                             if (contador1 == 2) i1 = limite + 1;
                     }
  
-        if (contador1 == 1) p = true;
+            if (contador1 == 1) 
+            p = true;
             } 
         } 
      
